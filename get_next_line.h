37bed60ccc8751534c54bd1434a092f210a035b9
plouvel <plouvel@student.42.fr>
2022-02-01 14:08:22 +0100

    fully working and protected GNL.

---
+++ get_next_line.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 20:07:41 by plouvel           #+#    #+#             */
+/*   Updated: 2022/01/31 15:38:06 by plouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+typedef enum e_flags
+{
+	INIT = 1U,
+	LINE_DONE = 1U << 1,
+	CAN_READ = 1U << 2,
+	MALLOC_EXCEPTION = 1U << 3
+}				t_flags;
+
+typedef struct s_gnl
+{
+	char	*buffer;
+	char	*start_buffer_addr;
+	char	*line;
+	char	*new_line;
+	char	tmp_char;
+	int		fd;
+	ssize_t	readed;
+	int		flags;
+}				t_gnl;
+
+char	*init_gnl(t_gnl *gnl, int fd);
+char	*quit_gnl(t_gnl *gnl);
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*get_next_line(int fd);
+
+#endif
